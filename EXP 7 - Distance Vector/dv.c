4776cfa6513e2a07f427bf955ffc893ca2e34c93
n0nsense-404 <abhiragh0@gmail.com>
2024-06-30 12:38:09 +0530

    change distance vector program

--- EXP 7 - Distance Vector/dv.c
+++ EXP 7 - Distance Vector/dv.c
@@ -1,52 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-struct node {
-    unsigned dist[20];
-    unsigned from[20];
-} rt[10];
+struct Link{
+    int hop,dest,wt;
+};
 
-int main() {
-    int costmat[20][20];
-    int nodes, i, j, k, count = 0;
+struct Network{
+    int H,L;
+    struct Link *link; 
+};
 
-    printf("\nEnter The Number of nodes: \n");
-    scanf("%d", &nodes);
+void main(){
+    int H,L,S,i,j,dist[H];
+    
+    printf("Enter No of Hops: ");
+    scanf("%d",&H);
 
-    // Inputting the cost matrix and initializing routing table
-    for (i = 0; i < nodes; i++) {
-        for (j = 0; j < nodes; j++) {
-            printf("\nEnter The Cost Matrix [%d][%d]: ", i, j);
-            scanf("%d", &costmat[i][j]);
-            rt[i].dist[j] = costmat[i][j];
-            rt[i].from[j] = j;
+    printf("Enter Number of Links: ");
+    scanf("%d",&L);
+
+    struct Network* n = (struct Network*) malloc(sizeof(struct Network));
+    n -> H = H;
+    n -> link = (struct Link *) malloc( n-> L *sizeof(struct Link));
+
+    for(i=0;i<L;i++){
+        printf("\nLink %d: Enter Source, Destination and weight \n", i+1);
+        scanf("%d",&n->link[i].hop);
+        scanf("%d",&n->link[i].dest);
+        scanf("%d",&n->link[i].wt);
+    }
+
+    for (i=0;i<H;i++)
+        dist[i] = INT_MAX;
+    
+    dist[S] = 0;
+
+    for(i=1;i<H;i++){
+        for(j=0;j<L;j++){
+            int u = n-> link[j].hop;
+            int v = n-> link[j].dest;
+            int wt = n-> link[j].wt;
+
+            if(dist[u] + wt < dist[v])
+                dist[v] = dist[u] + wt;
         }
     }
 
-    // Distance Vector Algorithm
-    do {
-        count = 0;
-        for (i = 0; i < nodes; i++) {     // for each node i
-            for (j = 0; j < nodes; j++) { // for each node j
-                for (k = 0; k < nodes; k++) { // for each node k
-                    if (rt[i].dist[j] > costmat[i][k] + costmat[k][j]) {
-                        rt[i].dist[j] = costmat[i][k] + costmat[k][j];
-                        rt[i].from[j] = k;
-                        count++;
-                    }
-                }
-            }
-        }
-    } while (count != 0);
+    for(i=0;i<L;i++){
+        int u = n-> link[i].hop;
+        int v = n-> link[i].dest;
+        int wt = n-> link[i].wt;
 
-    // Printing the routing table
-    for (i = 0; i < nodes; i++) {
-        printf("\n\nfor router %d\n", i + 1);
-        for (j = 0; j < nodes; j++) {
-            printf("\tnode %d via %d distance %d\n", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
-        }
+        if(dist[u] + wt < dist[v])
+            printf("Network contains negative weight cycle\n");
     }
 
-    printf("\n");
-
-    return 0;
+    printf("\nHop\tDistance from source\n");
+    for(i=0;i<H;i++){
+        printf("%d \t%d\n",i,dist[i]);
+    }
 }
